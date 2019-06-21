84eac7ee856a63bd5c95c2bcad3b3dee7f7dfbed
ORTEGA LUCAS <S.ORTEGA@CIMMYT.ORG>
2019-06-21 14:23:27 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <demo></demo>
+  </div>
+</template>
+<script>
+import './assets/libs/myLib.umd.js'
+export default {
+  props:{
+
+  },
+  name: 'myLib',
+  components: {
+    demo: myLib
+  }
+}
+</script>
+<style>
+</style>
